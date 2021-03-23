f5d4d52ea5865f1d14f1056f6894c0d5ec503d95
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-03-23 11:25:31 +0500

    title updated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './components/pages/Home';
 import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import NotFound from './components/pages/NotFound';
 
 function App() {
 
@@ -23,7 +24,7 @@ function App() {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
-
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
